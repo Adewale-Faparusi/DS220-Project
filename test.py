dbe54c7cf3f90347298da8277c74b46ca5994c33
Adewale-Faparusi <153222748+Adewale-Faparusi@users.noreply.github.com>
2023-12-12 23:48:12 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ data=pd.read_csv("titanic.csv")
 '''access the data set
 #We want to first learn about the data
 Shows the first 5 values of a dataset'''
-#print(data.head())
+print(data.head())
 
 
 '''find the datatypes'''
-# print(data.info())
+print(data.info())
 
 '''Clean the data by dropping Cabin, Age, and Embarked because not all values are non-null'''
-# data.drop(['Age','Embarked','Cabin'], axis=1, inplace=True)
-# print(data.info())
+data.drop(['Age','Embarked','Cabin'], axis=1, inplace=True)
+print(data.info())
 
 '''Q1 How many people lived or died?'''
-#(data['Survived'])
-# print(pd.value_counts(data['Survived']))
+(data['Survived'])
+print(pd.value_counts(data['Survived']))
 
 '''Over 50% of people on the boat died so before applying any other features 
 you're are more likely to die on the titanic'''
