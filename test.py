c2a668ada20cfc8fdb8c5667c26b06b6e62d869f
Adewale-Faparusi <153222748+Adewale-Faparusi@users.noreply.github.com>
2023-12-12 23:47:24 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
 import pandas as pd 
 data=pd.read_csv("titanic.csv")
-#access the data set
+
+'''access the data set
 #We want to first learn about the data
-
+Shows the first 5 values of a dataset'''
 #print(data.head())
-#shows the first 5 values of a dataset
 
-#find the datatypes
-print(data.info())
 
-#Q1 How many people lived or died?
+'''find the datatypes'''
+# print(data.info())
+
+'''Clean the data by dropping Cabin, Age, and Embarked because not all values are non-null'''
+# data.drop(['Age','Embarked','Cabin'], axis=1, inplace=True)
+# print(data.info())
+
+'''Q1 How many people lived or died?'''
 #(data['Survived'])
+# print(pd.value_counts(data['Survived']))
+
+'''Over 50% of people on the boat died so before applying any other features 
+you're are more likely to die on the titanic'''
+
+'''Q2 What is the difference of survival rate of men and women'''
+print(pd.value_counts(data['Sex']))
+ 
+
+ 
